6b62a7eb0e8752227716ab0759230b2f443e9609
ms.safari <ms.safari@outlook.com>
2023-05-22 12:32:59 +0300

    get query string has been implemented

--- src/main/java/com/safari/task/servlet/HelloServletAnnotation.java
+++ src/main/java/com/safari/task/servlet/HelloServletAnnotation.java
@@ -17,12 +17,28 @@ import java.io.IOException;
         }
 )
 public class HelloServletAnnotation extends HttpServlet {
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-                resp.setContentType("text/html");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
 
-                var writer = resp.getWriter();
-                String responseBody = "<html><body><h1>Hi :D</h1></body></html>";
-                writer.println(responseBody);
+        var writer = resp.getWriter();
+        writer.println("<html><body>");
+
+        var responseMessage = "Hello";
+
+        if (req.getRequestURI().equals("/hi")) {
+            responseMessage = "Hi";
         }
+
+        writer.println(String.format("<h1>%s :D</h1>", responseMessage));
+
+        var parameterMap = req.getParameterMap();
+
+        String[] ids = parameterMap.get("id");
+        for (var id : ids) {
+            writer.println(String.format("<h2>id: %s</h2>", id));
+        }
+
+        writer.println("</body></html>");
+    }
 }
